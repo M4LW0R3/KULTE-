6b72eb010580a9d0ce72d34d96f47da50ffe59d3
B.Karanja <youngkhalifah98@gmail.com>
2019-01-28 09:13:24 +0300

    scripts.js modified & index file indented

---
+++ scripts.js
@@ -0,0 +1,92 @@
+TweenMax.from(".account", 1.6, {
+    delay: 0.8,
+    opacity: 0,
+    y: 20,
+    ease: Expo.easeInOut
+});
+
+TweenMax.from(".logo", 1.6, {
+    delay: 1,
+    opacity: 0,
+    y: 20,
+    ease: Expo.easeInOut
+});
+
+TweenMax.from(".search", 1.4, {
+    delay: 1.2,
+    opacity: 0,
+    y: 20,
+    ease: Expo.easeInOut
+});
+
+TweenMax.from(".menu-btn", 1.4, {
+    delay: 1.4,
+    opacity: 0,
+    y: 20,
+    ease: Expo.easeInOut
+});
+
+TweenMax.from(".contact", 1.4, {
+    delay: 1,
+    opacity: 0,
+    y: 40,
+    ease: Expo.easeInOut
+});
+
+TweenMax.from(".tag", 1.4, {
+    delay: 1,
+    opacity: 0,
+    y: -40,
+    ease: Expo.easeInOut
+});
+
+TweenMax.from(".e-shop", 2, {
+    delay: 1.6,
+    opacity: 0,
+    y: 40,
+    ease: Expo.easeInOut
+});
+
+TweenMax.from(".year", 2, {
+    delay: 1.6,
+    opacity: 0,
+    x: -40,
+    ease: Expo.easeInOut
+});
+
+TweenMax.from(".add", 2, {
+    delay: 1,
+    opacity: 0,
+    x: -40,
+    rotation: 90,
+    ease: Expo.easeInOut
+});
+
+TweenMax.from(".arrow-left", 2, {
+    delay: 2.8,
+    opacity: 0,
+    x: 40,
+    ease: Expo.easeInOut
+});
+
+TweenMax.from(".arrow-right", 2, {
+    delay: 2.8,
+    opacity: 0,
+    x: -40,
+    ease: Expo.easeInOut
+});
+
+
+TweenMax.from(".line", 2, {
+    delay: 2.8,
+    opacity: 0,
+    x: -30,
+    ease: Expo.easeInOut
+});
+
+TweenMax.staggerFrom(".media ul li", 2, {
+    delay: 2.8,
+    opacity: 0,
+    y: 40,
+    ease: Expo.easeInOut
+}, 0.2);
